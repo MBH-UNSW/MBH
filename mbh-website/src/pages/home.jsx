88b0cfadf2f3c88e9b439a2d814679fa149d5b52
agnes-tjokrosetio <agnes.tjokrosetio@gmail.com>
2025-11-29 15:36:49 +1100

    nearly there

--- mbh-website/src/pages/home.jsx
+++ mbh-website/src/pages/home.jsx
@@ -2,6 +2,11 @@ import { useNavigate, useLocation } from "react-router-dom";
 import React from "react";
 import NavigationBar from "../components/navigation";
 import Footer from "../components/footer";
+import MBHOfficial from "../assets/images/team official.png";
+import AboutUs from "../assets/images/stairs.png";
+import OurTeam from "../assets/images/fun team.jpg";
+import Events from "../assets/images/vip.png";
+import Sponsor from "../assets/images/sponsor.png";
 
 function Home() {
   React.useEffect(() => window.scrollTo(0, 0), []);
@@ -10,8 +15,12 @@ function Home() {
     <>
       <NavigationBar />
       <div className="w-full flex flex-col pt-20">
-        <div className="w-full h-[40vh] bg-[#c50102] text-white flex justify-center items-center text-center text-4xl px-10">
-          <b>UNSW Mending Broken Hearts</b>
+        <div
+          className="relative w-full h-[50vh] bg-center bg-cover bg-no-repeat text-white flex justify-center items-center text-center text-4xl px-10"
+          style={{ backgroundImage: `url(${MBHOfficial})`, backgroundPositionY: "45%" }}
+        >
+          <div className="absolute inset-0 bg-black/20"></div>
+          <b className="relative z-10">UNSW Mending Broken Hearts</b>
         </div>
         <div className="w-full h-[auto] bg-[#f5f5f5] flex flex-col px-10 py-15 gap-6">
           <div className="text-2xl px-6">
@@ -32,27 +41,37 @@ function Home() {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
             <div
               onClick={() => navigate("/about-us")}
-              className="bg-white text-[#c50102] flex justify-center items-center font-bold text-xl p-12 hover:bg-[#f5f5f5] hover:text-[#c50102] transition aspect-square"
+              className="relative bg-center bg-cover bg-no-repeat text-white flex justify-center items-center font-bold text-xl p-12 hover:opacity-90 transition aspect-square rounded-lg overflow-hidden"
+              style={{ backgroundImage: `url(${AboutUs})` }}
             >
-              <span className="inline-block w-40 text-center pb-6 border-b-4 border-[#c50102]">About Us</span>
+              <div className="absolute inset-0 bg-black/30 z-0"></div>
+              <span className="relative z-10 inline-block w-40 text-center pb-6 border-b-4 border-white">About Us</span>
             </div>
             <div
               onClick={() => navigate("/our-team")}
-              className="bg-white text-[#c50102] flex justify-center items-center font-bold text-xl p-12 hover:bg-[#f5f5f5] hover:text-[#c50102] transition aspect-square"
+              className="relative bg-center bg-cover bg-no-repeat text-white flex justify-center items-center font-bold text-xl p-12 hover:opacity-90 transition aspect-square rounded-lg overflow-hidden"
+              style={{ backgroundImage: `url(${OurTeam})` }}
             >
-              <span className="inline-block w-40 text-center pb-6 border-b-4 border-[#c50102]">Our Team</span>
+              <div className="absolute inset-0 bg-black/30 z-0"></div>
+              <span className="relative z-10 inline-block w-40 text-center pb-6 border-b-4 border-white">Our Team</span>
             </div>
             <div
               onClick={() => navigate("/events")}
-              className="bg-white text-[#c50102] flex justify-center items-center font-bold text-xl p-12 hover:bg-[#f5f5f5] hover:text-[#c50102] transition aspect-square"
+              className="relative bg-center bg-cover bg-no-repeat text-white flex justify-center items-center font-bold text-xl p-12 hover:opacity-90 transition aspect-square rounded-lg overflow-hidden"
+              style={{ backgroundImage: `url(${Events})` }}
             >
-              <span className="inline-block w-40 text-center pb-6 border-b-4 border-[#c50102]">Events</span>
+              <div className="absolute inset-0 bg-black/30 z-0"></div>
+              <span className="relative z-10 inline-block w-40 text-center pb-6 border-b-4 border-white">Events</span>
             </div>
             <div
               onClick={() => navigate("/sponsorship")}
-              className="bg-white text-[#c50102] flex justify-center items-center font-bold text-xl p-12 hover:bg-[#f5f5f5] hover:text-[#c50102] transition aspect-square"
+              className="relative bg-center bg-cover bg-no-repeat text-white flex justify-center items-center font-bold text-xl p-12 hover:opacity-90 transition aspect-square rounded-lg overflow-hidden"
+              style={{ backgroundImage: `url(${Sponsor})` }}
             >
-              <span className="inline-block w-40 text-center pb-6 border-b-4 border-[#c50102]">Sponsorship</span>
+              <div className="absolute inset-0 bg-black/30 z-0"></div>
+              <span className="relative z-10 inline-block w-40 text-center pb-6 border-b-4 border-white">
+                Sponsorship
+              </span>
             </div>
           </div>
         </div>
